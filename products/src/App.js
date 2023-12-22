166b0fcb325429e92642f43d10c3f1bfb158db47
Trickster <reone054@gmail.com>
2023-12-22 08:37:57 +0545

    new_update

--- products/src/App.js
+++ products/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react';
+import { ProductsTable } from './components/ProductsTable.jsx';
 function App() {
+  const[search, setSearch] = useState('');
+  const[inStockONly,setinStockOnly] = useState(false);
+
+  const handleChange = (e) =>{
+    setSearch(e.target.value);
+  };
+  const toggleInStock = (e) => {
+    setinStockOnly(e.target.checked);
+  }
+  const products = [
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+    ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={handleChange} value={search} placeholder='Search...' />
+      <div>
+        <input id="checkbox" type="checkbox" onChange={toggleInStock} checked={inStockONly}/> 
+        <label htmlFor="checkbox">Show Only products in stockm</label>
+      </div>
+      <ProductsTable category="Fruits" products={products}/>
     </div>
   );
 }
---
+++ products/src/components/ProductsTable.jsx
@@ -0,0 +1,28 @@
+
+
+export const ProductsTable = (props) =>{
+    const category = props.category
+    const products = props.products
+    console.log(products)
+    return (<div>
+        <h3>{category}</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {products.map((products) => {return(
+                <tr>
+                    <td>{products.name}</td>
+                    <td>{products.price}</td>
+                </tr>
+                );
+                })}
+            </tbody>
+        </table>
+    </div>
+    );
+};
